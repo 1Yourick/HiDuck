0a9e7c4130cc11c73b6ca10b98b86a8790d9695b
Yourick <dev.yourick@gmail.com>
2021-09-28 16:59:25 +0300

    error image

--- app/src/main/java/com/yourick/hiduck/VideoActivity.java
+++ app/src/main/java/com/yourick/hiduck/VideoActivity.java
@@ -32,9 +32,6 @@ public class VideoActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         mWebView=(WebView)findViewById(R.id.videoview); //          то что было   <html><body>Promo video<br><iframe width="420" height="315" src="https://youtu.be/P4Xr6uY-bjc" frameborder="0" allowfullscreen></iframe></body></html>
-        //int width = getWindowManager().getDefaultDisplay().getWidth()/3;
-        //int height = getWindowManager().getDefaultDisplay().getHeight()/3;
-
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels/4;
@@ -53,23 +50,7 @@ public class VideoActivity extends AppCompatActivity {
         ws.setJavaScriptEnabled(true);
         mWebView.loadData(videoStr, "text/html", "utf-8");
 
-        /*Intent intent ;
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=Hxy8BZGQ5Jo")));*/ //открывает ютуб
 
-        /*int x = 2;
-
-        String frameVideo = "<html><body>Video From YouTube<br><iframe width=\\\"420\\\" height=\\\"315\\\" src=\\\"https://www.youtube.com/embed/47yJ2XCRLZs\\\" frameborder=\\\"0\\\" allowfullscreen></iframe></body></html>";
-
-        WebView displayYoutubeVideo = (WebView) findViewById(R.id.mWebView);
-        displayYoutubeVideo.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        WebSettings webSettings = displayYoutubeVideo.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        displayYoutubeVideo.loadData(frameVideo, "text/html", "utf-8");*/
     }
 
 
